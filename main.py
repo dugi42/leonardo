b60868e9d34e83e4d8695421ed4982c936d27a64
Daniel Hauser <danielhauser@Daniels-MacBook-Air.local>
2023-08-28 02:27:40 +0200

    main file added

---
+++ main.py
@@ -0,0 +1,15 @@
+
+# This code runs the app
+
+
+from src.common import create_parser, read_config
+from src.app import run_app
+
+parsed_args = create_parser()  # Create parsed arguments
+config = read_config(parsed_args.config)  # Get config data
+app = run_app(config)  # Run app including design engine as backend
+
+
+if __name__ == "__main__":
+    # Run app
+    app.run_server()
